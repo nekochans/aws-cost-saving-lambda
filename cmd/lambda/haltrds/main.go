8ad01e62d1a45400a1c97c2beb897e4a179386e6
keitakn <keita.koga.work@gmail.com>
2021-12-04 00:46:44 +0900

    :recycle: #1 Change the name to TARGET_CLUSTER_LIST because it is confusing

--- cmd/lambda/haltrds/main.go
+++ cmd/lambda/haltrds/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
 
@@ -9,10 +10,22 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type TargetRdsClusterList struct {
+	TargetClusterList []string `json:"targetClusterList"`
+}
+
 func HandleRequest(ctx context.Context, request events.CloudWatchEvent) error {
 	log.Println("🐱")
-	log.Println(os.Getenv("TARGET_CLUSTER_LIST"))
+	log.Println(os.Getenv("TARGET_RDS_CLUSTER_LIST"))
 	log.Println("🐱")
+
+	var targetRdsClusterList TargetRdsClusterList
+	if err := json.Unmarshal([]byte(os.Getenv("TARGET_RDS_CLUSTER_LIST")), &targetRdsClusterList); err != nil {
+	}
+
+	log.Println("🐹")
+	log.Println(targetRdsClusterList)
+	log.Println("🐹")
 
 	return nil
 }
